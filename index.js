bbd44dbaea53279bd7d1c01654f0410e583f0aef
abdullah ahmed <abdallaroom25@gmail.com>
2025-01-29 10:44:13 +0200

    add some routes

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 import express from "express"
 
 import connectDb from "./models/config.js"
-import compoundRouter from "./routers/compound.router.js"
-import apartmentRouter from "./routers/apartment.router.js"
+
+import homeRouter from "./routers/home.js"
 import userRouter from "./routers/user.router.js"
 import cors from "cors"
 import cookieParser from "cookie-parser"
@@ -23,9 +23,11 @@ app.use(cookieParser())
 // app.use("/compound", compoundRouter);
 // app.use("/apartment", apartmentRouter);
 app.use("/api/v1/user", userRouter);
+app.use("/", homeRouter);
 // Routes
 connectDb()
 // connection
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening to port ${port}`));
 
+ 
