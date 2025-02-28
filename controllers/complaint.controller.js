95d428ea05aaae953fd264aa1df956fc7e8ef637
abdullah ahmed <abdallaroom25@gmail.com>
2025-02-28 22:53:18 +0200

    update get_all_complaint controller

--- controllers/complaint.controller.js
+++ controllers/complaint.controller.js
@@ -8,16 +8,11 @@ import Complaint from "../models/Complaints.schema.js"
 
 export const getAllComplaints = async (req,res)=>{
   try {
-     const {userId} = req.params
-      if(!mongoose.Types.ObjectId.isValid(userId)){
-        return res.status(401).json({message:"invalid id "})
-      }
+    
 
-     const user = await User.findById(userId)
-     if(!user){
-      return res.status(401).json({message:"user not found"})
-     }
-
+     const allComplaints = await Complaint.find({}).sort({createdAt:-1}).populate("user")
+    
+    return res.status(200).json(allComplaints)
   } catch (error) {
     console.log(error)
   }
